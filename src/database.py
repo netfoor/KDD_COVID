37ac025dfbb62abcfe091f488b4c8c3df1c3673a
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2026-05-20 02:01:09 -0600

    feat: Download 7 years of data for analysis

---
+++ src/database.py
@@ -0,0 +1,16 @@
+import os 
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def get_mongo_client():
+
+    client = MongoClient(os.getenv("MONGO_URI"))
+    return client
+
+def get_database():
+    client = get_mongo_client()
+    db_name = os.getenv("MONGO_DB_NAME")
+    return client[db_name]
